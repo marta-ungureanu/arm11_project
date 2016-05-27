f99ffda21eb6eb85deb89a288c9ef868abb51405
mz4715@ic.ac.uk <mz4715@ic.ac.uk>
2016-05-27 19:13:38 +0100

    Double-checked branch, removed futile comments and fixed indentation issues

--- src/branch.c
+++ src/branch.c
@@ -3,7 +3,7 @@
 
 void branch(uint32_t instruction) {
 	if(!checkConditionField(instruction)) {
-		return ;
+		return;
 	}
 	int signBit = 0;
 	int32_t offset = instruction & TWENTYFOUR_BIT_MASK;
@@ -12,5 +12,5 @@ void branch(uint32_t instruction) {
 	}
 	offset *= 4;
 	offset += signBit;
-	ARM.registers[PC] += offset -4;
+	ARM.registers[PC] += offset - 4;
 }
