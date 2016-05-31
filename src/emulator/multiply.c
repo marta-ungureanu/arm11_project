0880bfed69ea084ff9d43b888a4dbe7209ce8d54
mu515 <mu515@ic.ac.uk>
2016-05-31 17:05:41 +0100

    Multiply function documented

--- src/emulator/multiply.c
+++ src/emulator/multiply.c
@@ -1,9 +1,27 @@
+/* ARM Project 2016
+ *
+ * multiply.c contains the function that executes a multiply instruction
+ *
+ * Group 3
+ * Members: abp14, oc1115, mu515, mz4715
+ */
+
 #include "emulator_misc.h"
 #include "armStructure.h"
 
-// function that executes the multiply instructions
+/* Function that executes a multiply instruction
+ *
+ * PARAM: uint32_t instruction
+ * 32 bit binary representation of the instruction to be executed
+ *
+ * RETURN: void
+ *
+ * Depending on the accumulate bit, the function performs the multiplication
+ * Rd := Rm x Rs or Rd := Rm x Rs + Rn. If the condition bit is set, the CPSR
+ * flags N and Z are updated.
+ */
 void multiply(uint32_t instruction) {
-	// if the condition does not hold, the instruction is not executed
+
 	if(!checkConditionField(instruction)) {
 		return;
 	}
@@ -15,9 +33,6 @@ void multiply(uint32_t instruction) {
 	uint32_t rs = (instruction & MUL_SD_REG_MASK3) >> 8;
 	uint32_t rm = instruction & MUL_SD_REG_MASK4;
 
-	/* depending on the accumulate bit, the instruction performs 
-	 * a multiply and accumulate, or a multiply only
-	 */
 	if(accumulator) {
 		ARM.registers[rd] = ARM.registers[rm] * ARM.registers[rs] 
 				  + ARM.registers[rn];
@@ -25,11 +40,9 @@ void multiply(uint32_t instruction) {
 		ARM.registers[rd] = ARM.registers[rm] * ARM.registers[rs];
 	}
 
-	// if the set condition bit is set, the CPRS flags are updated
 	if(setCondition) {
 		uint32_t Nflag = ARM.registers[rd] & FLAG_N_MASK;
 
-		// setting N flag
 		if((ARM.registers[CPSR] & FLAG_N_MASK) != Nflag) {
 			if(Nflag) {
 				ARM.registers[CPSR] += FLAG_N_MASK;
@@ -38,11 +51,9 @@ void multiply(uint32_t instruction) {
 			}
 		}
 
-		// setting Z flag
-		if(ARM.registers[rd] == 0) {
-			if((ARM.registers[CPSR] & FLAG_Z_MASK) == 0) {
-				ARM.registers[CPSR] += FLAG_Z_MASK;
-			}
+		if(ARM.registers[rd] == 0 &&
+				(ARM.registers[CPSR] & FLAG_Z_MASK) == 0) {
+			ARM.registers[CPSR] += FLAG_Z_MASK;
 		}
 	}
 }
